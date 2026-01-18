01b67fc15bfced1fc48ead960a16c14dec82403e
sultanbekalibek <sultanbekalibek15@gmail.com>
2026-01-18 23:07:21 +0500

    Sultanbek

--- Lecture5JDBCExample-master/src/com/company/controllers/interfaces/IUserController.java
+++
@@ -1,7 +0,0 @@
-package com.company.controllers.interfaces;
-
-public interface IUserController {
-    String createUser(String name, String surname, String gender);
-    String getUser(int id);
-    String getAllUsers();
-}
